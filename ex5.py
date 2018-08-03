c423c652d6dc9615d3e44be2669e6fdb7751c9a7
azwinabdul <amusoli@ymail.com>
2018-08-03 18:19:02 +0700

    new commit 030818

---
+++ ex5.py
@@ -0,0 +1,26 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+my_height_cm = my_height * 2.54
+my_weight_kg = my_weight / 0.45
+
+
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"My height in centimeters is {my_height_cm}")
+print(f"My weight in kilogram is {my_weight_kg}")
+print("Actually that's not to heavy")
+print(f"he's got {my_eyes} eyes and {my_hair} hair. ")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If i add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+
