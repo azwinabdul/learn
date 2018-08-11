5e074b8b3cbc1636009996b827940231a98ab0e1
azwinabdul <amusoli@ymail.com>
2018-08-11 11:06:12 +0700

    ex6, slow learn

--- ex5.py
+++ ex5.py
@@ -1,21 +1,21 @@
 my_name = 'Zed A. Shaw'
 my_age = 35 #not a lie
 my_height = 74 #inches
-my_weight = 180 #lbs
+my_weight = 200 #lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
 my_height_cm = my_height * 2.54
-my_weight_kg = my_weight / 0.45
+my_weight_kg = my_weight / 2.2
 
 
 
 print(f"Let's talk about {my_name}.")
 print(f"He's {my_height} inches tall.")
 print(f"He's {my_weight} pounds heavy.")
-print(f"My height in centimeters is {my_height_cm}")
-print(f"My weight in kilogram is {my_weight_kg}")
+print(f"My height in centimeters is {my_height_cm} cm.")
+print(f"My weight in kilogram is {my_weight_kg} kg.")
 print("Actually that's not to heavy")
 print(f"he's got {my_eyes} eyes and {my_hair} hair. ")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
---
+++ ex6.py
@@ -0,0 +1,30 @@
+#variable of people who understand binary
+types_of_people = 10
+#variable in variable string
+x = f"There are {types_of_people} types of people."
+#variable of string and testing string on variable
+binary = "binary"
+do_not = "don't"
+#string on variable
+y = f"Those who know {binary} and those who {do_not}."
+
+#print x and y as it is
+print(x)
+print(y)
+#pring x and y in string
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+#boolean variable
+hilarious = False
+
+#string variable with possibilty have variable inside
+joke_evaluation = "Isn't that joke so funny?! {} "
+
+#trying to print variable and insert some variable before print it
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+#add string while printing the variable
+print(w + e)
